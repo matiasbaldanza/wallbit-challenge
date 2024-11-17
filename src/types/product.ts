c0289f356e0366f3b763850a16349f85e34e03cc
Matias Baldanza <matiasbaldanza@gmail.com>
2024-11-17 14:14:10 -0300

    update ProductSchema to match FakeStoreAPI specs

--- src/types/product.ts
+++ src/types/product.ts
@@ -3,8 +3,14 @@ import { z } from 'zod'
 export const ProductSchema = z.object({
   id: z.number(),
   title: z.string(),
+  description: z.string(),
+  category: z.string(),
   price: z.number(),
   image: z.string().url(),
+  rating: z.object({
+    rate: z.number(),
+    count: z.number()
+  })
 })
 
 export type Product = z.infer<typeof ProductSchema>
