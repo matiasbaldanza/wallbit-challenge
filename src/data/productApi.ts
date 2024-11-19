572259ea7827437cb9124a70237e2b54015ef541
Matias Baldanza <matiasbaldanza@gmail.com>
2024-11-18 21:43:44 -0300

    feat: add  'product not found' condition to fetchAndValidateProduct as per FakeStoreApi

--- src/data/productApi.ts
+++ src/data/productApi.ts
@@ -10,10 +10,18 @@ const fetchAndValidateProduct = async <T>(
 ): Promise<T> => {
   try {
     const response = await axios.get(url)
+
+    // FakeStoreAPI returns status code 200 with an empty
+    // string for response.data for non-existent products 
+    if (!response.data || response.data === '') {
+      throw new Error('Product not found.')
+    }
+
+    // Validate the response data with Zod schema
     return schema.parse(response.data)
   } catch (error) {
     console.error('API error:', error)
-    throw new Error('Failed to fetch data')
+    throw new Error('Failed to fetch product data')
   }
 }
 
