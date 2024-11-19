b8092e2999bc61b77b97d06869e313a018d6b067
Matias Baldanza <matiasbaldanza@gmail.com>
2024-11-19 18:39:40 -0300

    fix: remove react-markdown. It broke Vite's build (unable to resolve import '#minFile' from '.../node_modules/vfile/lib/index.js'

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { siteStrings } from '@/siteStrings'
 import { Helmet } from 'react-helmet-async'
-import ReactMarkdown from 'react-markdown'
 import { ArrowUpRightIcon } from 'lucide-react'
 
 export const Layout = (
@@ -27,25 +26,24 @@ export const Layout = (
           <p className='text-sm text-gray-500'>
             {siteStrings.footer.copyright.replace('{year}', new Date().getFullYear().toString())}
           </p>
-          {/* Format links from markdown and make them open in a new tab */}
-          <ReactMarkdown
+          <p
             className='text-xs text-gray-500'
-            components={{
-              a: ({ children, href }) => {
-                const text = React.Children.toArray(children).join('')
-                return (
-                  <a
-                    href={href}
-                    target='_blank' rel='noopener noreferrer' className='text-blue-500 hover:underline'>
-                    {text}
-                    <ArrowUpRightIcon className='inline mb-0.5 w-3 h-3' />
-                  </a>
-                )
-              }
-            }}
           >
-            {siteStrings.footer.author}
-          </ReactMarkdown>
+            Hecho por{" "}
+            <a
+              href="https://twitter.com/matiasbaldanza"
+              target='_blank' rel='noopener noreferrer' className='text-blue-500 hover:underline'>
+              MatíasBaldanza
+              <ArrowUpRightIcon className='inline mb-0.5 w-3 h-3' />
+            </a>{" "}
+            para el challenge de Frontend de Wallbit - {" "}
+            <a
+              href="https://github.com/matiasbaldanza/wallbit-challenge"
+              target='_blank' rel='noopener noreferrer' className='text-blue-500 hover:underline'>
+              REPO
+              <ArrowUpRightIcon className='inline mb-0.5 w-3 h-3' />
+            </a>
+          </p>
         </footer>
       </div>
     </div>
